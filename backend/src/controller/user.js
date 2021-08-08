fe802692d935deb18a3012c049de80036516cdee
akhil-hari <aklhnrpm@gmail.com>
2021-08-08 21:25:16 +0530

    src/controller/user.js

--- backend/src/controller/user.js
+++ backend/src/controller/user.js
@@ -1,7 +1,7 @@
 const userSchedule=require('../models/userSchedule')
 
 async function userScheduleStatus(id){
-    data = await userSchedule.find({user:id}).sort({timestamp:-1}).catch(err=>{console.log(`cant get from user shecdule due to following error '${err}'`)})
+    data = await userSchedule.find({user:id}).sort({timestamp:-1}).catch(err=>{console.log(`cant get from user shecdule due to following error '${err}'`)});
 
-
+    return (Promise.all([data]))[0];
 }
